3cf8dd012385c517cc95d68a579a96bff42d8bd6
GiorPan <panagiwtopoulos.giwrgos@gmail.com>
2015-09-02 00:14:50 +0300

    Create bin_list.hs
    infinite list of binary numbers

---
+++ bin_list.hs
@@ -0,0 +1,5 @@
+allbin = [0] : [1] : [ys | ys <- mybin (tail allbin)]
+mybin (x:xs) = if length x == sum x then [i-i|i<-[0..length x]]:mybin xs else if last x ==1 then 
+  ((take (length x - (length (takeWhile (==1) (reverse x)) +1)) x)
+    ++[1]++[i-i|i<-[1..(length  (takeWhile (==1) (reverse x) ) ) ]])
+      :mybin xs else (init x ++ [1]):mybin xs
