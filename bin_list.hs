5d622c6d59886edfce4a8b629aaaa53178ce8c05
GiorPan <panagiwtopoulos.giwrgos@gmail.com>
2015-09-02 10:50:35 +0300

    Update bin_list.hs

--- bin_list.hs
+++ bin_list.hs
@@ -1,3 +1,5 @@
+--infinite list of binary numbers increasing in value and length
+
 allbin = [0] : [1] : [ys | ys <- mybin (tail allbin)]
 mybin (x:xs) = if length x == sum x then [i-i|i<-[0..length x]]:mybin xs else if last x ==1 then 
   ((take (length x - (length (takeWhile (==1) (reverse x)) +1)) x)
